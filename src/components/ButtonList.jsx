8fd6e745578864c18ec9b8d40d1cdebcafb24de0
Ankitgif <ankitsinha2303@gmail.com>
2020-06-12 01:54:42 +0530

    [Ankit] Refactor code, added downloaded src  image

--- src/components/ButtonList.jsx
+++ src/components/ButtonList.jsx
@@ -1,31 +1,18 @@
 import React from 'react';
 import buttoncard from './Button';
-
+import scale from '../assets/scale.png';
+import hot from '../assets/hot.png';
+import beaker from '../assets/beaker.png';
 
 function buttonlist() {
-    return (
-      <div className="measurement_style">
+  return (
+    <div className="measurement_style">
 
-      <button id="length"><img src="https://img.icons8.com/ultraviolet/48/000000/length.png"/>Length</button>
-      <button id="temperature"><img src="https://img.icons8.com/color/48/000000/temperature.png"/>Temperature</button>
-      <button id="volume"><img src="https://img.icons8.com/dusk/48/000000/volume.png"/>Volume</button>
-        
-      </div>
-    );
-   }
-   export default buttonlist
-// const Buttonlist = (props) => {
-//     return (
-//         <div className="measurement_style" >
-//         <button id={props.id}>
-//             <img src={props.image} alt="measure"/>
-//             {props.name}
-//         </button>
-//         </div>
-//     )
-// }
-// export default Buttonlist;
+      <button id="length"><img src={scale} alt="scale" id="scale" />Length</button>
+      <button id="temperature"><img src={hot} alt="equipment to measure temperature" id="hot" />Temperature</button>
+      <button id="volume"><img src={beaker} alt="equipment to measure volume" id="beaker" />Volume</button>
 
-// <button label="Length"/>
-        // <button label="Temperature"/>
-        // <button label="Volume"/>
+    </div>
+  );
+}
+export default buttonlist
