84508dd36516ba682d41fb42c00b94b9e7ce89c0
Ankitgif <ankitsinha2303@gmail.com>
2020-06-12 02:01:03 +0530

    [Ankit] Added InputBox component

---
+++ src/components/InputBox.jsx
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+
+class InputBox extends Component {
+    render() {
+        return (
+            <div className="inputbox">
+                <div className="boxone">
+                    <input type="" value="1" id="inputone"></input>
+                    <span>
+                        <select className="selectone">
+                            <option selected="1">Meters</option>
+                            <option>Kilometre</option>
+                            <option>Centimetres</option>
+                            <option>Milimetre</option>
+                            <option>Micrometre</option>
+                            <option>Mile</option>
+                            <option>Foot</option>
+                        </select>
+                    </span>
+                </div>
+                <div className="boxtwo">
+                    <input type="" value="100" id="inputtwo"></input>
+                    <span>
+                        <select className="selecttwo">
+                            <option selected="100">Centimeters</option>
+                            <option>Kilometre</option>
+                            <option>Metres</option>
+                            <option>Centimetres</option>
+                            <option>Milimetre</option>
+                            <option>Micrometre</option>
+                            <option>Mile</option>
+                            <option>Foot</option>
+                        </select>
+                    </span>
+                </div>
+            </div>
+        )
+    }
+}
+export default InputBox
+
+// class InputBox extends Component {
+//     render() {
+//         return (
+//             <div className="inputbox">
+//                 <div className="boxone">
+//                     <input type="" value="1" id="inputone"></input>
+//                     <span>
+//                     </span>
+//                     <input type="" value="100" id="inputtwo"></input><br />
+//                         <select id="selectone">
+//                             <option selected="1">Meters</option>
+//                             <option>Kilometre</option>
+//                             <option>Centimetres</option>
+//                             <option>Milimetre</option>
+//                             <option>Micrometre</option>
+//                             <option>Mile</option>
+//                             <option>Foot</option>
+//                         </select>
+//                         <span>
+//                         </span>
+//                         <select id="selecttwo">
+
+//                             <option selected="100">Centimeters</option>
+//                             <option>Kilometre</option>
+//                             <option>Metres</option>
+//                             <option>Centimetres</option>
+//                             <option>Milimetre</option>
+//                             <option>Micrometre</option>
+//                             <option>Mile</option>
+//                             <option>Foot</option>
+//                         </select>
+
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
