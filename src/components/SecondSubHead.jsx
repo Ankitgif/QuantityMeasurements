b94481dc4d0d0831c40b8dcb77360ad4e3e887d9
Ankitgif <ankitsinha2303@gmail.com>
2020-06-11 14:41:31 +0530

    [Ankit] Added SecondSubHead component

---
+++ src/components/SecondSubHead.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SecondSubHead extends Component {
+    render() {
+        return (<div className="second-subhead-style">
+            <h2 id="from">FROM</h2>
+            <h2 id="to">TO</h2>
+        </div>
+        )
+    }
+}
+
+export default SecondSubHead
