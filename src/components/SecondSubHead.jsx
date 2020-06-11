4ad60da37a4a2901f8b9c700e56f74886d670d8c
Ankitgif <ankitsinha2303@gmail.com>
2020-06-12 01:58:32 +0530

    [Ankit] Added second sub heading component

--- src/components/SecondSubHead.jsx
+++ src/components/SecondSubHead.jsx
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 class SecondSubHead extends Component {
     render() {
         return (<div className="second-subhead-style">
-            <h2 id="from">FROM</h2>
-            <h2 id="to">TO</h2>
+            <h2 className="from">FROM</h2>
+            <h2 className="to">TO</h2>
         </div>
         )
     }
