060d14ba4afff67af80eaebf1f9abc4729124743
Ankitgif <ankitsinha2303@gmail.com>
2020-06-19 01:10:02 +0530

    [Ankit] Added react router

--- src/components/Quantity.jsx
+++ src/components/Quantity.jsx
@@ -4,21 +4,36 @@ import SubHead from './SubHead';
 import ButtonList from './ButtonList';
 import SecondSubHead from './SecondSubHead';
 import '../css/styles.css';
-//import InputBox from './InputBox';
-import InputBox from './Converter';
+//import InputBox from './InputBox'
+import InputBox from './Converter'
+import DisplayButtons from './DisplayButtons';
+import ConvertTemperature from './ConvertTemp';
+import ConvertVolume from './ConvertVolume';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import ConvertLength from './ConvertLength';
+
 
 class Quantity extends Component {
     render() {
         return (
+            <BrowserRouter>
             <div>
                 <Header />
                 <div className="content">
                     <SubHead />
-                    <ButtonList />
+                    <ButtonList /> 
+                 {/* <DisplayButtons /> */} 
                     <SecondSubHead />
-                    <InputBox />
+                   {/* <InputBox /> */}
+                  {/*  <Route path="/inputbox" exact component={Converter} /> */}
+                  <Route path="/" exact component={ConvertLength} />
+                  <Route path="/convertlength" exact component={ConvertLength} />
+                    <Route path="/converttemp" exact component={ConvertTemperature} />
+                    <Route path="/convertvolume" exact component={ConvertVolume} />
+                    
                 </div>
             </div>
+            </BrowserRouter>
         )
     }
 }
