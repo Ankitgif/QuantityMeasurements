256ae5b61cef364073d07c5edff7867a47c97b9d
Ankitgif <ankitsinha2303@gmail.com>
2020-06-19 01:18:09 +0530

    [Ankit] Added Link from react-router-dom

--- src/components/ButtonList.jsx
+++ src/components/ButtonList.jsx
@@ -3,14 +3,17 @@ import buttoncard from './Button';
 import scale from '../assets/scale.png';
 import hot from '../assets/hot.png';
 import beaker from '../assets/beaker.png';
+import ConvertTemp from './ConvertTemp';
+import {Link} from 'react-router-dom';
+import {Button} from 'reactstrap'
 
 function buttonlist() {
   return (
     <div className="measurement_style">
 
-      <button id="length"><img src={scale} alt="scale" id="scale" />Length</button>
-      <button id="temperature"><img src={hot} alt="equipment to measure temperature" id="hot" />Temperature</button>
-      <button id="volume"><img src={beaker} alt="equipment to measure volume" id="beaker" />Volume</button>
+    <Link to="/ConvertLength"><Button id="length"><img src={scale} alt="scale" id="scale"/><a>Length</a></Button></Link>
+    <Link to="/ConvertTemp"><Button id="temperature"><img src={hot} alt="equipment to measure temperature" id="hot" /><a>Temperature</a></Button></Link>
+    <Link to="/ConvertVolume"><Button id="volume"><img src={beaker} alt="equipment to measure volume" id="beaker" /><a>Volume</a></Button></Link>
 
     </div>
   );
