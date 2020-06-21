1dbae1a24ff2cb3784a57018539bde65b5d13e56
Ankitgif <ankitsinha2303@gmail.com>
2020-06-21 13:49:11 +0530

    [Ankit] Refactored code, changed from class component to functional

--- src/components/Quantity.jsx
+++ src/components/Quantity.jsx
@@ -1,42 +1,33 @@
-import React, { Component } from 'react';
 import Header from './Header';
 import SubHead from './SubHead';
 import ButtonList from './ButtonList';
 import SecondSubHead from './SecondSubHead';
 import '../css/styles.css';
-//import InputBox from './InputBox'
-import InputBox from './Converter'
-import DisplayButtons from './DisplayButtons';
 import ConvertTemperature from './ConvertTemp';
 import ConvertVolume from './ConvertVolume';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ConvertLength from './ConvertLength';
 
+import React from 'react'
 
-class Quantity extends Component {
-    render() {
-        return (
-            <BrowserRouter>
+function Quantity() {
+    return (
+        <BrowserRouter>
             <div>
                 <Header />
                 <div className="content">
                     <SubHead />
-                    <ButtonList /> 
-                 {/* <DisplayButtons /> */} 
+                    <ButtonList />
                     <SecondSubHead />
-                   {/* <InputBox /> */}
-                  {/*  <Route path="/inputbox" exact component={Converter} /> */}
-                  <Route path="/" exact component={ConvertLength} />
-                  <Route path="/convertlength" exact component={ConvertLength} />
+                    <Route path="/" exact component={ConvertLength} />
+                    <Route path="/convertlength" exact component={ConvertLength} />
                     <Route path="/converttemp" exact component={ConvertTemperature} />
                     <Route path="/convertvolume" exact component={ConvertVolume} />
-                    
+
                 </div>
             </div>
-            </BrowserRouter>
-        )
-    }
+        </BrowserRouter>
+    )
 }
-
 
 export default Quantity
